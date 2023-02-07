ccebdc4a1ae7a2a9bc9f2125e3a720af4eb85cf7
PR0FESS0R 99 <124684607+pr0fess0r99@users.noreply.github.com>
2023-02-07 21:09:09 +0530

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -1,62 +1,57 @@
-"""lokaman"""
+"""pr0fess0r99"""
 from pyrogram.types import (InlineKeyboardButton, InlineKeyboardMarkup,ForceReply)
 from pyrogram import Client , filters
 
 @Client.on_callback_query(filters.regex('upgrade'))
 async def upgrade(bot,update):
 	text = """**Free Plan User**
-	Daily  Upload limit 2GB
+	Daily Upload Limit 2GB
 	Price 0
 	
 	**VIP 1 ** 
-	Daily  Upload  limit 10GB
+	Daily Upload  Limit 10GB
 	Price Rs 55  🇮🇳/🌎 0.67$  per Month
 	
 	**VIP 2 **
-	Daily Upload limit 50GB
+	Daily Upload Limit 50GB
 	Price Rs 80  🇮🇳/🌎 0.97$  per Month
-	
-	**VIP3**
-	Daily Upload limit 100GB
-	Price Rs 150  🇮🇳/🌎 1.81$  per Month
-	
-	
-	Pay Using Upi I'd ```9480251952@paytm```
+
+	UPI 🆔 Details
+
+        Google pay 📲 ```joynathnet4@oksbi```
+        Phonepe 📲 ```bijoy.nath@ybl```
 	
 	After Payment Send Screenshots Of 
         Payment To Admin"""
 	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/mrlokaman")], 
-        			[InlineKeyboardButton("PayPal 🌎",url = "https://www.paypal.me/lokamanchendekar"),
-        			InlineKeyboardButton("Paytm",url = "https://p.paytm.me/xCTH/los89jy0")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+        			InlineKeyboardButton("👮 Admin", url = "https://t.me/pr0fess0r99")], 
+        			[InlineKeyboardButton("Cancel ✖️", callback_data = "cancel")  ]])
 	await update.message.edit(text = text,reply_markup = keybord)
 	
 
 @Client.on_message(filters.private & filters.command(["upgrade"]))
 async def upgradecm(bot,message):
 	text = """**Free Plan User**
-	Daily  Upload limit 2GB
+	Daily Upload Limit 2GB
 	Price 0
 	
 	**VIP 1 ** 
-	Daily  Upload  limit 10GB
+	Daily Upload  Limit 10GB
 	Price Rs 55  🇮🇳/🌎 0.67$  per Month
 	
 	**VIP 2 **
-	Daily Upload limit 50GB
+	Daily Upload Limit 50GB
 	Price Rs 80  🇮🇳/🌎 0.97$  per Month
-	
-	**VIP3**
-	Daily Upload limit 100GB
-	Price Rs 150  🇮🇳/🌎 1.81$  per Month
-	
-	
-	Pay Using Upi I'd ```9480251952@paytm```
+
+	UPI 🆔 Details
+
+        Google pay 📲 ```joynathnet4@oksbi```
+        Phonepe 📲 ```bijoy.nath@ybl```
 	
 	After Payment Send Screenshots Of 
         Payment To Admin"""
 	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/mrlokaman")], 
-        			[InlineKeyboardButton("PayPal 🌎",url = "https://www.paypal.me/lokamanchendekar"),
-        			InlineKeyboardButton("Paytm",url = "https://p.paytm.me/xCTH/los89jy0")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
-	await message.reply_text(text = text,reply_markup = keybord)
+        			InlineKeyboardButton("👮 Admin", url = "https://t.me/pr0fess0r99")], 
+        			[InlineKeyboardButton("Cancel ✖️", callback_data = "cancel")  ]])
+	await update.message.edit(text = text,reply_markup = keybord)
+	
