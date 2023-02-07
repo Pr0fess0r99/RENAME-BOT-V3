6011625bbd73d70609d335e22e15bb56de3bdef3
PR0FESS0R 99 <124684607+pr0fess0r99@users.noreply.github.com>
2023-02-07 20:01:17 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -10,4 +10,4 @@ async def start(client,message):
 	data = find_one(int(botid))
 	total_rename = data["total_rename"]
 	total_size = data["total_size"]
-	await message.reply_text(f"Total User:- {total_user()}\nBot :- @rename_urbot\nCreater :- @mrlokaman\nLanguage :-Python3\nLibrary :- Pyrogram 2.0\nServer :- Railway\nTotal Renamed File :-{total_rename}\nTotal Size Renamed :- {humanbytes(int(total_size))} ",quote=True)
+	await message.reply_text(f"Total User:- {total_user()}\n🤖 Bot Name: Rename Bot\n👨‍💻 Developer: @pr0fess0r99\n㊗️ Language: Python3\n🛠️ Library: Pyrogram 2.0\n📡 Server: Heroku\n📝 Total Renamed File :-{total_rename}\n💾 Total Size Renamed :- {humanbytes(int(total_size))} ",quote=True)
