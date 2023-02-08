a81f96278179a9ebcbdf0830061e27dedd8f657f
PR0FESS0R 99 <124684607+pr0fess0r99@users.noreply.github.com>
2023-02-08 12:11:33 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -18,17 +18,6 @@ log_channel = int(os.environ.get("LOG_CHANNEL",""))
 token = os.environ.get('TOKEN','')
 botid = token.split(':')[0]
 
-#Part of Day --------------------
-currentTime = datetime.datetime.now()
-
-if currentTime.hour < 12:
-	wish = "Good Morning."
-elif 12 <= currentTime.hour < 12:
-	wish = 'Good Afternoon.'
-else:
-	wish = 'Good Evening.'
-
-#-------------------------------
 
 @Client.on_message(filters.private & filters.command(["start"]))
 async def start(client,message):
@@ -37,13 +26,13 @@ async def start(client,message):
 	    id = message.text.split(' ')[1]
 	except:
 	    await message.reply_text(text =f"""
-	Hello 👋 {wish} {message.from_user.first_name},\n
-	__I Am File Rename Bot, Please Sent Me Any Telegram 
-	**Document Or Video** And Enter New Filename To Rename It__
+	Hello 🙋‍♂ {message.from_user.first_name},\nI'm File Rename Bot, Please Sent Me Any Telegram 
+	**Document Or Video** And Enter New Filename To Rename It.
 	""",reply_to_message_id = message.id ,  
 	reply_markup=InlineKeyboardMarkup([[
-	   InlineKeyboardButton("🔗 Support", url="https://t.me/TechProjectsChats") 
+	   InlineKeyboardButton("👨‍💻 Developer", url="https://t.me/pr0fess0r99") 
            ],[
+           InlineKeyboardButton("🔗 Support", url="https://t.me/TechProjectsChats")
            InlineKeyboardButton("📢 Updates", url="https://t.me/TechProjectsUpdates") ]  ]))
 	    return
 	if id:
@@ -51,13 +40,13 @@ async def start(client,message):
 	        try:
 	            await client.send_message(id,"Your Frind Alredy Using Our Bot")
 	            await message.reply_text(text =f"""
-	Hello 👋 {wish} {message.from_user.first_name},\n
-	__I Am File Rename Bot, Please Sent Me Any Telegram 
-	**Document Or Video** And Enter New Filename To Rename It__
+	Hello 🙋‍♂ {message.from_user.first_name},\nI Am File Rename Bot, Please Sent Me Any Telegram 
+	**Document Or Video** And Enter New Filename To Rename It.
 	""",reply_to_message_id = message.id ,  
 	reply_markup=InlineKeyboardMarkup([[
-	   InlineKeyboardButton("🔗 Support", url="https://t.me/TechProjectsChats") 
+	   InlineKeyboardButton("👨‍💻 Developer", url="https://t.me/pr0fess0r99") 
            ],[
+           InlineKeyboardButton("🔗 Support", url="https://t.me/TechProjectsChats")
            InlineKeyboardButton("📢 Updates", url="https://t.me/TechProjectsUpdates") ]  ]))
 	        except:
 	             return
@@ -68,13 +57,13 @@ async def start(client,message):
 	         new_limit = limit + 104857600
 	         uploadlimit(int(id),new_limit)
 	         await message.reply_text(text =f"""
-	Hello 👋 {wish} {message.from_user.first_name},\n
-	__I Am File Rename Bot, Please Sent Me Any Telegram 
-	**Document Or Video** And Enter New Filename To Rename It__
+	Hello 🙋‍♂ {message.from_user.first_name},\nI Am File Rename Bot, Please Sent Me Any Telegram 
+	**Document Or Video** And Enter New Filename To Rename It.
 	""",reply_to_message_id = message.id ,  
 	reply_markup=InlineKeyboardMarkup([[
-	   InlineKeyboardButton("🔗 Support", url="https://t.me/TechProjectsChats") 
+	   InlineKeyboardButton("👨‍💻 Developer", url="https://t.me/pr0fess0r99") 
            ],[
+           InlineKeyboardButton("🔗 Support", url="https://t.me/TechProjectsChats")
            InlineKeyboardButton("📢 Updates", url="https://t.me/TechProjectsUpdates") ]  ]))
 	         
 
@@ -88,7 +77,7 @@ async def send_doc(client,message):
        	try:
        		await client.get_chat_member(update_channel, user_id)
        	except UserNotParticipant:
-       		await message.reply_text("**__You Are Not Subscribed My Updates Channel__** ",
+       		await message.reply_text("**You Are Not Subscribed My Updates Channel**",
        		reply_to_message_id = message.id,
        		reply_markup = InlineKeyboardMarkup(
        		[ [ InlineKeyboardButton("🔔 SUBSCRIBE 🔔" ,url=f"https://t.me/{update_channel}") ]   ]))
@@ -121,7 +110,7 @@ async def send_doc(client,message):
        conversion = datetime.timedelta(seconds=left)
        ltime = str(conversion)
        if left > 0:       	    
-       	await message.reply_text(f"```Sorry Dude I Am Not Only For YOU\nFlood Control Is Active So Please Wait For {ltime}```",reply_to_message_id = message.id)
+       	await message.reply_text(f"```Sorry Dude I Am Not Only For YOU 🤗\nFlood Control Is Active ☑️ So Please Wait For ⏰ {ltime}```",reply_to_message_id = message.id)
        else:
        		# Forward a single message
            		
@@ -151,14 +140,14 @@ async def send_doc(client,message):
        		            return
        		        pre_check = check_expi(buy_date)
        		        if pre_check == True:
-       		            await message.reply_text(f"""__What Do You Want Me To Do With This File ?__\n**File Name**: {filename}\n**File Size**: {humanize.naturalsize(file.file_size)}\n**Dc ID**: {dcid}""",reply_to_message_id = message.id,reply_markup = InlineKeyboardMarkup([[ InlineKeyboardButton("📝 Rename",callback_data = "rename"),InlineKeyboardButton("✖️ Cancel",callback_data = "cancel")  ]]))
+       		            await message.reply_text(f"""What Do You Want Me To Do With This File ?\n**File Name**: {filename}\n**File Size**: {humanize.naturalsize(file.file_size)}\n**Dc ID**: {dcid}""",reply_to_message_id = message.id,reply_markup = InlineKeyboardMarkup([[ InlineKeyboardButton("📝 Rename",callback_data = "rename"),InlineKeyboardButton("✖️ Cancel",callback_data = "cancel")  ]]))
        		            total_rename(int(botid),prrename)
        		            total_size(int(botid),prsize,file.file_size)
        		        else:
        		            uploadlimit(message.from_user.id,2147483648)
        		            usertype(message.from_user.id,"Free")
 	
-       		            await message.reply_text(f'Your Plane Expired On {buy_date}',quote=True)
+       		            await message.reply_text(f'Your Plane Expired On 📅 {buy_date}',quote=True)
        		            return
        		    else:
        		          	await message.reply_text("You Can't Upload File Bigger Than 2GB")
@@ -176,5 +165,5 @@ async def send_doc(client,message):
        		    total_size(int(botid),prsize,file.file_size)
        		    await message.reply_text(f"""__What do you want me to do with this file?__\n**File Name** :- {filename}\n**File Size** :- {filesize}\n**Dc ID** :- {dcid}""",reply_to_message_id = message.id,reply_markup = InlineKeyboardMarkup(
        		[[ InlineKeyboardButton("📝 Rename",callback_data = "rename"),
-       		InlineKeyboardButton("Cancel ✖️",callback_data = "cancel")  ]]))
+       		InlineKeyboardButton("Cancel ❎",callback_data = "cancel")  ]]))
        		
