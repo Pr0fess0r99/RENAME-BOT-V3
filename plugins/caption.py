b1e7a9be0cf354ba37d56806ec7ef091d60790c9
PR0FESS0R 99 <124684607+pr0fess0r99@users.noreply.github.com>
2023-02-08 20:32:16 +0530

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -5,7 +5,7 @@ from helper.database import *
 @Client.on_message(filters.private & filters.command('set_caption'))
 async def add_caption(client, message):
     if len(message.command) == 1:
-       return await message.reply_text("**Give Me a Caption To Set.\n\nExample:- `/set_caption File Name`**")
+       return await message.reply_text("**Give Me a Caption To Set.\n\nExample: `/set_caption 📂 File Name: {filename} 💾 File Size: {filesize} ⏰ Duration: {duration}`**")
     caption = message.text.split(" ", 1)[1]
     addcaption(int(message.chat.id), caption)
     await message.reply_text("**Your Caption Successfully Added ☑️**")
