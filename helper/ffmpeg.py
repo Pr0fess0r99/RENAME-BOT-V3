4a0c480b0fd91db0ba0eb46aa28e73fc56276d21
Viizetabdi <154399342+Viizetabdi@users.noreply.github.com>
2023-12-21 08:41:57 +0300

    Update ffmpeg.py

--- helper/ffmpeg.py
+++ helper/ffmpeg.py
@@ -5,6 +5,10 @@ from PIL import Image
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 
+async def chunk_size(length: int):
+    return 2 ** max(min(math.ceil(math.log2(length / 1024)), 10), 2) * 1024
+
+
 async def fix_thumb(thumb):
     width = 739
     height = 415
