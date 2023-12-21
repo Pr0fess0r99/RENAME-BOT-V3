8947f7b4badc2aa965065fb29f5b9542cfb6d36c
Viizetabdi <154399342+Viizetabdi@users.noreply.github.com>
2023-12-21 08:45:14 +0300

    Update ffmpeg.py

--- helper/ffmpeg.py
+++ helper/ffmpeg.py
@@ -9,46 +9,19 @@ async def chunk_size(length: int):
     return 2 ** max(min(math.ceil(math.log2(length / 1024)), 10), 2) * 1024
 
 
-async def fix_thumb(thumb):
-    width = 739
-    height = 415
-    try:
-        if thumb != None:
-            metadata = extractMetadata(createParser(thumb))
-            if metadata.has("width"):
-                width = metadata.get("width")
-            if metadata.has("height"):
-                height = metadata.get("height")
-                Image.open(thumb).convert("RGB").save(thumb)
-                img = Image.open(thumb)
-                img.resize((320, height))
-                img.save(thumb, "JPEG")
-    except Exception as e:
-        print(e)
-        thumb = None 
-       
-    return width, height, thumb
-    
-async def take_screen_shot(video_file, output_directory, ttl):
-    out_put_file_name = f"{output_directory}/{time.time()}.jpg"
-    file_genertor_command = [
-        "ffmpeg",
-        "-ss",
-        str(ttl),
-        "-i",
-        video_file,
-        "-vframes",
-        "1",
-        out_put_file_name
-    ]
-    process = await asyncio.create_subprocess_exec(
-        *file_genertor_command,
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
-    )
-    stdout, stderr = await process.communicate()
-    e_response = stderr.decode().strip()
-    t_response = stdout.decode().strip()
-    if os.path.lexists(out_put_file_name):
-        return out_put_file_name
-    return None
+async def offset_fix(offset, chunksize):
+    offset -= offset % chunksize
+    return offset
+
+
+async def fix_thumbnail(thumb_path: str):
+    metadata = extractMetadata(createParser(thumb_path))
+    if metadata.has("height"):
+        height = metadata.get("height")
+    else:
+        height = 0
+    Image.open(thumb_path).convert("RGB").save(thumb_path)
+    img = Image.open(thumb_path)
+    img.resize((320, height))
+    img.save(thumb_path, "JPEG")
+    return thumb_path
