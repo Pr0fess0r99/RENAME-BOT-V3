44fe86e9fd99f1c5bad7019d6262c7315997554c
Viizetabdi <154399342+Viizetabdi@users.noreply.github.com>
2023-12-22 11:03:24 +0300

    Update ffmpeg.py

--- helper/ffmpeg.py
+++ helper/ffmpeg.py
@@ -1,54 +1,84 @@
-import time
-import os
-import asyncio
-from PIL import Image
-from hachoir.metadata import extractMetadata
-from hachoir.parser import createParser
+#  This file is part of the VIDEOconvertor distribution.
+#  Copyright (c) 2021 vasusen-code ; All rights reserved. 
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, version 3.
+#
+#  This program is distributed in the hope that it will be useful, but
+#  WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+#  General Public License for more details.
+#
+#  License can be found in < https://github.com/vasusen-code/VIDEOconvertor/blob/public/LICENSE> .
 
-async def fix_thumb(thumb):
-    try:
-        if thumb is not None:
-            metadata = extractMetadata(createParser(thumb))
-            if metadata and metadata.has("width") and metadata.has("height"):
-                width = metadata.get("width")
-                height = metadata.get("height")
+import os, time, subprocess, asyncio
 
-                with Image.open(thumb) as img:
-                    img.convert("RGB").save(thumb, "JPEG")
+from datetime import datetime as dt
+from telethon import events
+from ethon.telefunc import fast_download
+from ethon.pyfunc import video_metadata
 
-                    # Maintain aspect ratio
-                    aspect_ratio = height / width
-                    new_height = int(415 * aspect_ratio)
+def hhmmss(seconds):
+    x = time.strftime('%H:%M:%S',time.gmtime(seconds))
+    return x
 
-                    img = img.resize((415, new_height))
-                    img.save(thumb, "JPEG")
-
-                return 415, new_height, thumb
-    except Exception as e:
-        print(f"Error in fix_thumb: {e}")
-    
-    return None, None, None
-    
-async def take_screen_shot(video_file, output_directory, ttl):
-    out_put_file_name = f"{output_directory}/{time.time()}.jpg"
-    file_genertor_command = [
-        "ffmpeg",
-        "-ss",
-        str(ttl),
-        "-i",
-        video_file,
-        "-vframes",
-        "1",
-        out_put_file_name
-    ]
+async def ssgen(video, time_stamp):
+    out = dt.now().isoformat("_", "seconds") + ".jpg"
+    cmd = ["ffmpeg",
+           "-ss",
+           f"{hhmmss(time_stamp)}", 
+           "-i",
+           f"{video}",
+           "-frames:v",
+           "1", 
+           f"{out}",
+           "-y"
+          ]
     process = await asyncio.create_subprocess_exec(
-        *file_genertor_command,
+        *cmd,
         stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE
     )
+        
     stdout, stderr = await process.communicate()
-    if stderr:
-        print(f"Error in take_screen_shot: {stderr.decode().strip()}")
-    if os.path.lexists(out_put_file_name):
-        return out_put_file_name
-    return None
+    x = stderr.decode().strip()
+    y = stdout.decode().strip()
+    if os.path.isfile(out):
+        return out
+    else:
+        None       
+        
+async def screenshot(event, msg):
+    Drone = event.client
+    name = dt.now().isoformat("_", "seconds") + ".mp4"
+    edit = await Drone.send_message(event.chat_id, "Trying to process.", reply_to=msg.id)
+    if hasattr(msg.media, "document"):
+        file = msg.media.document
+    else:
+        file = msg.media
+    if msg.file.name:
+        name = msg.file.name
+    try:
+        await fast_download(name, file, Drone, edit, time.time(), "**DOWNLOADING:**")
+    except Exception as e:
+        print(e)
+        return await edit.edit(f"An error occured while downloading.") 
+    pictures = []
+    captions = []
+    n = [8, 7, 6, 5, 4, 3, 2, 1.5, 1.25, 1.10]
+    duration = (video_metadata(name))["duration"]
+    for i in range(10):
+        sshot = await ssgen(name, duration/n[i]) 
+        if sshot is not None:
+            pictures.append(sshot)
+            captions.append(f'screenshot at {hhmmss(duration/n[i])}')
+            await edit.edit(f"`{i+1}` screenshot generated.")
+    if len(pictures) > 0:
+        await Drone.send_file(event.chat_id, pictures, caption=captions)
+    else:
+        await edit.edit("No screenshots could be generated!")
+    await edit.delete()
+    for pic in pictures:
+        os.remove(pic)
+    os.remove(name)
