acd2732656906e2b912cd77e73f240f0e2e76644
Viizetabdi <154399342+Viizetabdi@users.noreply.github.com>
2023-12-22 20:01:43 +0300

    Update progress.py

--- helper/progress.py
+++ helper/progress.py
@@ -28,7 +28,7 @@ async def progress_for_pyrogram(
             ''.join(["○" for i in range(20 - math.floor(percentage / 5))]),
             round(percentage, 2))
 
-        tmp = progress + "{0} of {1}\n**Speed**: {2}/s\n**ETA**: {3}\n".format(
+        tmp = progress + "{0} of {1}\n**Speed**: {2}/s\n**ESTIMATED TIME**: {3}\n {2}/s\n**ESTIMATED TIME**: {3}\n".format(
             humanbytes(current),
             humanbytes(total),
             humanbytes(speed),
