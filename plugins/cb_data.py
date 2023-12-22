4c545ea2de3f1fc19f39eac5353263738f21d65a
Viizetabdi <154399342+Viizetabdi@users.noreply.github.com>
2023-12-22 11:54:03 +0300

    Update cb_data.py

--- plugins/cb_data.py
+++ plugins/cb_data.py
@@ -40,7 +40,7 @@ async def rename(bot,update):
 	chat_id = update.message.chat.id
 	id = update.message.reply_to_message_id
 	await update.message.delete()
-	await update.message.reply_text(f"Please Enter The New Filename\nOr Copy 
+	await update.message.reply_text(f"Please Enter The New Filename\🔺nOr Copy 
 Paste Old File Name\n\n✍️ ✍️ Note:- .mp4 Extension is required",reply_to_message_id = id,
 	reply_markup=ForceReply(True) )
 	dateupdate(chat_id,date)
