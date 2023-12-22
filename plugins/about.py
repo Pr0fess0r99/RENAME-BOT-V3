740153e25b8590b310a17cbe151b5b117cc5f180
Viizetabdi <154399342+Viizetabdi@users.noreply.github.com>
2023-12-22 15:16:23 +0300

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -1,13 +1,16 @@
 import os 
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, InputMediaPhoto
 from pyrogram import Client, filters
 token = os.environ.get('TOKEN','')
 botid = token.split(':')[0]
 from helper.database import botdata, find_one, total_user
+
 from helper.progress import humanbytes
+
 @Client.on_message(filters.private & filters.command(["about"]))
 async def start(client,message):
 	botdata(int(botid))
 	data = find_one(int(botid))
 	total_rename = data["total_rename"]
 	total_size = data["total_size"]
-	await message.reply_text(f"👥 Total Users:- {total_user()}\n🤖 Bot Name: Rename Bot\n👨‍💻 Developer: @VIIZET\n㊗️ Language: Python3\n🛠️ Library: Pyrogram 2.0\n📡 Server: Heroku\n📝 Total Renamed File :-{total_rename}\n💾 Total Size Renamed :- {humanbytes(int(total_size))} ",quote=True)
+	await message.reply_text(f"᚛› 𝐔𝐬𝐞𝐫𝐬 - {total_user()}\n᚛› 𝐑𝐞𝐧𝐚𝐦𝐞 𝐅𝐢𝐥𝐞𝐬 - {total_rename}\n᚛› 𝐑𝐞𝐧𝐚𝐦𝐞𝐝 𝐒𝐢𝐳𝐞 - {humanbytes(int(total_size))}",quote=True)
