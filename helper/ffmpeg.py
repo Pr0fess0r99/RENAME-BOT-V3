471982fee43a11812352f6e8f128be07b9dcee18
Viizetabdi <154399342+Viizetabdi@users.noreply.github.com>
2023-12-22 18:45:29 +0300

    Update ffmpeg.py

--- helper/ffmpeg.py
+++ helper/ffmpeg.py
@@ -6,8 +6,8 @@ from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
 
 async def fix_thumb(thumb):
-    width = 0
-    height = 0
+    width = 415
+    height = 739
     try:
         if thumb != None:
             metadata = extractMetadata(createParser(thumb))
