47757ca4c83254a5be2b1f4dd1bc6c437ce87406
Viizetabdi <154399342+Viizetabdi@users.noreply.github.com>
2023-12-23 13:44:44 +0300

    Update upgrade.py

--- plugins/upgrade.py
+++ plugins/upgrade.py
@@ -5,53 +5,58 @@ from pyrogram import Client , filters
 @Client.on_callback_query(filters.regex('upgrade'))
 async def upgrade(bot,update):
 	text = """**Free Plan User**
-	Daily Upload Limit 2GB
+	Daily  Upload limit 1.2GB
 	Price 0
 	
-	**VIP 1 ** 
-	Daily Upload  Limit 10GB
-	Price  🇸🇴 0.67$  per Month
+	**🪙 Silver Tier 🪙** 
+	Daily  Upload  limit 10GB
+	Price Rs 66  ind /🌎 0.8$  per Month
 	
-	**VIP 2 **
-	Daily Upload Limit 50GB
-	Price 🇸🇴 0.97$  per Month
-
-	UPI 🆔 Details
-
-        Contect 📲 Tg @VIIZET
-        Phone.  📲 WAAFI
+	**💫 Gold Tier 💫**
+	Daily Upload limit 50GB
+	Price Rs 100  ind /🌎 1.2$  per Month
+	
+	**💎 Diamond 💎**
+	Daily Upload limit 100GB
+	Price Rs 206  ind /🌎 2.5$  per Month
+	
+	
+	Pay Using Upi I'd ```7808912076@paytm```
 	
 	After Payment Send Screenshots Of 
-        Payment To Admin"""
+        Payment To Admin @mRiderDM"""
 	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("👮 Admin", url = "https://t.me/Viizet")], 
-        			[InlineKeyboardButton("Cancel ✖️", callback_data = "cancel")  ]])
+        			InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/mRiderDM")], 
+        			[InlineKeyboardButton("Paytm",url = "https://p.paytm.me/xCTH/vo37hii9"),
+        			InlineKeyboardButton("Paytm",url = "https://p.paytm.me/xCTH/vo37hii9")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
 	await update.message.edit(text = text,reply_markup = keybord)
 	
 
 @Client.on_message(filters.private & filters.command(["upgrade"]))
 async def upgradecm(bot,message):
 	text = """**Free Plan User**
-	Daily Upload Limit 2GB
+	Daily  Upload limit 1.2GB
 	Price 0
 	
-	**VIP 1 ** 
-	Daily Upload  Limit 10GB
-	Price  🇸🇴 0.67$  per Month
+	**🪙 Silver Tier 🪙** 
+	Daily  Upload  limit 10GB
+	Price Rs 66  ind /🌎 0.8$  per Month
 	
-	**VIP 2 **
-	Daily Upload Limit 50GB
-	Price  🇸🇴 0.97$  per Month
-
-	UPI 🆔 Details
-
-        Contect 📲 tg @VIIZET
-        Phone.  📲 WAAFI
+	**💫 Gold Tier 💫**
+	Daily Upload limit 50GB
+	Price Rs 100  ind /🌎 1.2$  per Month
+	
+	**💎 Diamond 💎**
+	Daily Upload limit 100GB
+	Price Rs 206  ind /🌎 2.5$  per Month
+	
+	
+	Pay Using Upi I'd ```7808912076@paytm```
 	
 	After Payment Send Screenshots Of 
-        Payment To Admin"""
+        Payment To Admin @mRiderDM"""
 	keybord = InlineKeyboardMarkup([[ 
-        			InlineKeyboardButton("👮 Admin", url = "https://t.me/viizet")], 
-        			[InlineKeyboardButton("Cancel ✖️", callback_data = "cancel")  ]])
-	await update.message.edit(text = text,reply_markup = keybord)
-	
+        			InlineKeyboardButton("ADMIN 🛂",url = "https://t.me/mRiDerDM")], 
+        			[InlineKeyboardButton("Paytm",url = "https://p.paytm.me/xCTH/vo37hii9"),
+        			InlineKeyboardButton("Paytm",url = "https://p.paytm.me/xCTH/vo37hii9")],[InlineKeyboardButton("Cancel",callback_data = "cancel")  ]])
+	await message.reply_text(text = text,reply_markup = keybord)
